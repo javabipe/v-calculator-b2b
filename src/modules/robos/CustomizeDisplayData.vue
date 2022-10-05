be9c07465beaf6ae53b1a5c4403d6fb5b5e69b77
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-05 18:01:29 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -17,7 +17,7 @@
 
 <script>
 import { defineComponent, reactive } from "vue";
-import TableLite from "./components/TableLite.vue";
+import TableLite from "vue3-table-lite/ts";
 
 // Fake Data for 'asc' sortable
 const sampleData1 = (offst, limit) => {
