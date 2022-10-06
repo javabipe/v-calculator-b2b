6766071e299f73291224ff87debb8d2dd5c19bd6
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 15:50:03 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -13,14 +13,14 @@
 
     const CustomizeDisplayData = defineComponent({
         components: {
-            const headers: Header[] = [
+            headers: Header[] = [
                 { text: 'Name', value: 'name' },
                 { text: 'Height (cm)', value: 'height', sortable: true },
                 { text: 'Weight (kg)', value: 'weight', sortable: true },
                 { text: 'Age', value: 'age', sortable: true }
             ];
 
-            const items: Item[] = [
+            items: Item[] = [
                 { 'name': 'Curry', 'height': 178, 'weight': 77, 'age': 20 },
                 { 'name': 'James', 'height': 180, 'weight': 75, 'age': 21 },
                 { 'name': 'Jordan', 'height': 181, 'weight': 73, 'age': 22 }
