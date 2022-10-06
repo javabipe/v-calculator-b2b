637bc1ba91973cfcbb0a183fbbbafaaabbd956a4
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 16:13:57 -0300

    Update router.ts

--- src/core/lib/router.ts
+++ src/core/lib/router.ts
@@ -16,6 +16,7 @@ export default new Router({
         },
         {
             path: '/robos',
+            name: 'easydatatable',
             component: CustomizeDisplayData,
         },
         {
