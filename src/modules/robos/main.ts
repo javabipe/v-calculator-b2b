05524535558bb1d66a7284c6de7b60efe1bd4f20
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 13:56:41 -0300

    Delete main.ts

--- src/modules/robos/main.ts
+++
@@ -1,5 +0,0 @@
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
-
-const app = createApp(App);
-app.component('EasyDataTable', Vue3EasyDataTable);
