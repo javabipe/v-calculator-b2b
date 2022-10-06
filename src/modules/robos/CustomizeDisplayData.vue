f46fc7eed0e10fd92810cb46701629ce64b9b82f
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 14:53:00 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -1,11 +1,37 @@
 <template>
     <div class="view view--centered">
-        <router-view />
+        <EasyDataTable
+            :headers="headers"
+            :items="items"
+        />
     </div>
 </template>
 
 <script lang="ts">
     import { defineComponent } from '@vue/composition-api';
-    const AuthView = defineComponent({});
-    export default AuthView;
+    import type { Header, Item } from 'vue3-easy-data-table';
+
+    const CustomizeDisplayData = defineComponent({
+        setup() {
+            const headers: Header[] = [
+                { text: 'Name', value: 'name' },
+                { text: 'Height (cm)', value: 'height', sortable: true },
+                { text: 'Weight (kg)', value: 'weight', sortable: true },
+                { text: 'Age', value: 'age', sortable: true }
+            ];
+
+            const items: Item[] = [
+                { 'name': 'Curry', 'height': 178, 'weight': 77, 'age': 20 },
+                { 'name': 'James', 'height': 180, 'weight': 75, 'age': 21 },
+                { 'name': 'Jordan', 'height': 181, 'weight': 73, 'age': 22 }
+            ];
+
+            return {
+                headers,
+                items
+            };
+        },
+    });
+    export default CustomizeDisplayData;
 </script>
+<style lang="scss" src="vue3-easy-data-table/dist/style.css" />
