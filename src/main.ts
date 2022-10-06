391573e6f70b0deb7a06632af1d8586a26d46b27
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 15:02:30 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import '@/core/lib/filters';
 import './registerServiceWorker';
 import store from '@/core/store';
 import { auth } from '@/core/lib/firebase';
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 Vue.config.productionTip = false;
 
