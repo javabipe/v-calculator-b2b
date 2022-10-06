89e8563cb3ee655ab3bb0c59f4e23bcb03772019
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 11:26:13 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -1,30 +1,31 @@
 <template>
-  <EasyDataTable :headers="headers" :items="items"/>
+    <EasyDataTable :headers="headers" 
+    :items="items" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-import type { Header, Item } from "vue3-easy-data-table";
+    import { defineComponent } from '@vue/composition-api';
+    import type { Header, Item } from 'vue3-easy-data-table';
 
-export default defineComponent({
-  setup() {
-    const headers: Header[] = [
-      { text: "Name", value: "name" },
-      { text: "Height (cm)", value: "height", sortable: true },
-      { text: "Weight (kg)", value: "weight", sortable: true },
-      { text: "Age", value: "age", sortable: true }
-    ];
+    export default defineComponent({
+        setup() {
+            const headers: Header[] = [
+                { text: 'Name', value: 'name' },
+                { text: 'Height (cm)', value: 'height', sortable: true },
+                { text: 'Weight (kg)', value: 'weight', sortable: true },
+                { text: 'Age', value: 'age', sortable: true }
+            ];
 
-    const items: Item[] = [
-      { "name": "Curry", "height": 178, "weight": 77, "age": 20 },
-      { "name": "James", "height": 180, "weight": 75, "age": 21 },
-      { "name": "Jordan", "height": 181, "weight": 73, "age": 22 }
-    ];
+            const items: Item[] = [
+                { "name": 'Curry', 'height': 178, 'weight': 77, 'age': 20 },
+                { "name": 'James', 'height': 180, 'weight': 75, 'age': 21 },
+                { "name": 'Jordan', 'height': 181, 'weight': 73, 'age': 22 }
+            ];
 
-    return {
-      headers,
-      items
-    };
-  },
-});
+            return {
+                headers,
+                items
+            };
+        },
+    });
 </script>
