5ed193cbb01ca504892d999e458ca7eff0a1f089
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 15:54:59 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -12,15 +12,15 @@
     import type { Header, Item } from 'vue3-easy-data-table';
 
     const CustomizeDisplayData = defineComponent({
-        components: {
-            headers = [
+        setup() {
+            const headers: Header[] = [
                 { text: 'Name', value: 'name' },
                 { text: 'Height (cm)', value: 'height', sortable: true },
                 { text: 'Weight (kg)', value: 'weight', sortable: true },
                 { text: 'Age', value: 'age', sortable: true }
             ];
 
-            items = [
+            const items: Item[] = [
                 { 'name': 'Curry', 'height': 178, 'weight': 77, 'age': 20 },
                 { 'name': 'James', 'height': 180, 'weight': 75, 'age': 21 },
                 { 'name': 'Jordan', 'height': 181, 'weight': 73, 'age': 22 }
@@ -30,7 +30,7 @@
                 headers,
                 items
             };
-        }
+        },
     });
     export default CustomizeDisplayData;
 </script>
