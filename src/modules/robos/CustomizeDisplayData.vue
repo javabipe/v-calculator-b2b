70d36093be404104bdffbaf784e2970e02eb7f64
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-06 16:28:52 -0300

    Update CustomizeDisplayData.vue

--- src/modules/robos/CustomizeDisplayData.vue
+++ src/modules/robos/CustomizeDisplayData.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent } from '@vue/composition-api';
     import type { Header, Item } from 'vue3-easy-data-table';
 
     const CustomizeDisplayData = defineComponent({
